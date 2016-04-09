0be21df295ebf4a5921e98c4445b1828adc2b94a
Maciej Wilgucki <mwigucki@gmail.com>
2016-04-09 21:13:20 +0200

    small refactoring

--- src/Commands/Init.php
+++ src/Commands/Init.php
@@ -9,12 +9,19 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class Init extends Command
 {
+    protected $projectPath;
     protected $viewsPath = '/views';
     protected $layoutPath = '/_layouts';
     protected $partialsPath = '/_partials';
     protected $cachePath = '/cache';
     protected $outputPath = '/compiled';
     protected $publicPath = '/public';
+
+    public function __construct($name, $projectPath)
+    {
+        $this->projectPath = $projectPath;
+        parent::__construct($name);
+    }
 
     protected function configure()
     {
@@ -26,14 +33,14 @@ class Init extends Command
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $filesystem = new Filesystem();
-        $filesystem->makeDirectory(getcwd().$this->viewsPath, 0775, false, true);
-        $filesystem->makeDirectory(getcwd().$this->viewsPath.$this->layoutPath, 0775, false, true);
-        $filesystem->makeDirectory(getcwd().$this->viewsPath.$this->partialsPath, 0775, false, true);
-        $filesystem->makeDirectory(getcwd().$this->cachePath, 0775, false, true);
-        $filesystem->makeDirectory(getcwd().$this->outputPath, 0775, false, true);
-        $filesystem->makeDirectory(getcwd().$this->publicPath, 0775, false, true);
+        $filesystem->makeDirectory($this->projectPath.$this->viewsPath, 0775, false, true);
+        $filesystem->makeDirectory($this->projectPath.$this->viewsPath.$this->layoutPath, 0775, false, true);
+        $filesystem->makeDirectory($this->projectPath.$this->viewsPath.$this->partialsPath, 0775, false, true);
+        $filesystem->makeDirectory($this->projectPath.$this->cachePath, 0775, false, true);
+        $filesystem->makeDirectory($this->projectPath.$this->outputPath, 0775, false, true);
+        $filesystem->makeDirectory($this->projectPath.$this->publicPath, 0775, false, true);
 
-        $filesystem->copy(__DIR__.'/../index.php', getcwd().$this->publicPath.'/index.php');
+        $filesystem->copy(__DIR__.'/../index.php', $this->projectPath.$this->publicPath.'/index.php');
 
         $output->writeln("Initialization complete!");
     }

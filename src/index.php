93c51558f9f44c7387669ecb71ef7d5fc3b95ce3
Maciej Wilgucki <mwigucki@gmail.com>
2016-04-09 11:45:33 +0200

    bugfix - incorrect paths in index.php

--- src/index.php
+++ src/index.php
@@ -4,8 +4,8 @@ require __DIR__.'/../vendor/autoload.php';
 
 try {
     $app = new \BladeBuilder\App(
-        getcwd() . '/views',
-        getcwd() . '/cache'
+        __DIR__.'/../views',
+        __DIR__.'/../cache'
     );
     echo $app->renderView(trim($_SERVER['REQUEST_URI'], '/'));
 } catch (Exception $e) {

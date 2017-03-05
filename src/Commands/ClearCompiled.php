07b6a833e725a27fb0d5c964f840127aa346e002
Maciej Wilgucki <mwigucki@gmail.com>
2017-03-05 17:30:21 +1100

    clear cache and compiled files

--- src/Commands/ClearCompiled.php
+++ src/Commands/ClearCompiled.php
@@ -6,22 +6,31 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class Clear extends Command
+class ClearCompiled extends Command
 {
-    protected $cachePath;
-    protected $outputPath;
+    protected $compiledPath;
 
     public function __construct($name, $projectPath)
     {
-        $this->cachePath = $projectPath.'/cache';
-        $this->outputPath = $projectPath.'/compiled';
+        $this->compiledPath = $projectPath.'/compiled';
         parent::__construct($name);
     }
 
     protected function configure()
     {
         $this
-            ->setName('clear')
-            ->setDescription('Build html files based on blade views');
+            ->setName('clear:compiled')
+            ->setDescription('Clear compiled views that aren\'t present in views directory');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $di = new \DirectoryIterator($this->compiledPath);
+        foreach ($di as $filename => $file) {
+            if (!$file->isDot()) {
+                unlink($file->getPathname());
+            }
+        }
+        $output->writeln("Compiled files cleared");
     }
 }
--- tests/ClearCacheTest.php
+++ tests/ClearCacheTest.php
@@ -1,16 +1,38 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: mwilgucki
- * Date: 29/12/16
- * Time: 10:34 PM
- */
+namespace BladeBuilder\Tests;
 
-
+use org\bovigo\vfs\vfsStream;
 use BladeBuilder\Commands\ClearCache;
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Tester\CommandTester;
 
-
-class ClearCacheTest extends PHPUnit_Framework_TestCase
+class ClearCacheTest extends \PHPUnit_Framework_TestCase
 {
+    public function setUp()
+    {
+        vfsStream::setup('root', null, [
+            'cache' => [
+                md5(uniqid().mt_rand()).'.php' => ''
+            ],
+        ]);
+    }
 
+    public function testExecute()
+    {
+        $application = new Application();
+        $application->add(new ClearCache(null, vfsStream::url('root')));
+
+        $command = $application->find('clear:cache');
+        $commandTester = new CommandTester($command);
+        $commandTester->execute(['command' => $command->getName()]);
+
+        $di = new \DirectoryIterator(vfsStream::url('root/cache'));
+        $totalFiles = 0;
+        foreach ($di as $filename => $file) {
+            if (!$file->isDot()) {
+                $totalFiles++;
+            }
+        }
+        $this->assertTrue($totalFiles === 0);
+    }
 }
